38c53992ecd87b018a256e1b45c02f981ead0baa
ds94059 <ds94059@gmail.com>
2020-02-20 16:33:40 +0800

    include some package

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-// import Logo from "https://www.aaeon.com/emgz/client_logo.png";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-class Index extends React.Component{
-    render(){
-        return(
-            <div>
-                this is index.
-            </div>
-        );
-    }
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+//call dll function
+//console.log("EApiLibInitialize", window.Dll.EApiLibInitialize());
+
+const path32 = "/public/dll/32bit/aaeonEAPI.dll";
+const path64 = "/public/dll/64bit/aaeonEAPI.dll";
+
+const config = {
+  EApiLibInitialize: ["UInt32", []]
+};
+
+try {
+  const funcs = window.RegistDll(path32, config);
+  console.log("32bit : ", funcs.EApiLibInitialize());
+} catch (e) {
+  console.log("now is using 64bit nodejs");
 }
-export default Index;
-  // ========================================
-  
-  ReactDOM.render(
-   
-   <App />,
-    document.getElementById('root')
-  );
-  
+
+try {
+    const funcs = window.RegistDll(path64, config);
+    console.log("64bit : ", funcs.EApiLibInitialize());
+  } catch (e) {
+    console.log("now is using 32bit nodejs");
+  }
--- src/smbus.js
+++ src/smbus.js
@@ -11,23 +11,45 @@ class Smbus extends React.Component{
                 <Upbar/>
                 <div>
                     <div class="wholeleft">
-                        <fieldset class="wholeleft">
-                        <legend class="blueword">CPU Information</legend>
-                        <table>
-                            1
+                        <fieldset class="supa">
+                        <legend class="blueword">Smbus Setting</legend>
+                        <table class="smbuset">
+                            <table class="setImgMiddle"><img src="./image/smbus_set.png"></img></table>
+                            <p>Device Address:</p>
+                            <select class="fullwid"><option>0xA0</option></select>
+                            <p>Register Offset:</p>
+                            <select class="fullwid"><option>0xA0</option></select>
+                            <p>Data to write:</p>
+                            <p>LowByte <select class="disabled_block" disabled><option></option></select>  </p>
+                            <p>HighByte <select class="disabled_block" disabled><option></option></select>  </p>
                         </table>
                         </fieldset>   
                     </div>                    
                     <div class="wholeleft">
                         <fieldset class="upright">
-                        <legend class="blueword">Model Information</legend>
-                        <table>
-                            2
+                        <legend class="blueword">Operation Options</legend>
+                        <table class="smbuset">
+                            <tr>
+                                <td><input type="radio" name="byte"></input>Read Byte</td>
+                                <td><input type="radio" name="byte"></input>Write Byte</td>
+                            </tr>
+                            <br></br><br></br>
+                            <tr>
+                                <td><input type="radio" name="word"></input>Read Word</td>
+                                <td><input type="radio" name="word"></input>Write Word</td>
+                            </tr>
                         </table>
                         </fieldset>
-                        <fieldset class="downright">
-                        <table>3</table>
-                        </fieldset>
+                        <div class="downright">
+                        <table class="smbuset">
+                            <p>Receive Data: <input type="text"></input></p>
+                            <br></br>
+                            <p>Status: <input type="text" disabled></input></p>
+                            <br></br>
+                            <input type="button" value="Read" class="button"></input>                  
+                            <input type="button" value="Write" disabled class="button"></input>                    
+                        </table>
+                        </div>
                     </div>        
                     
                 </div>
